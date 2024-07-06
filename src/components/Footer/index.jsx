394636dcf04a41fbd7a8c4c3adf78e8e2e6da8ba
LiNkSlen <bagnikyana@yandex.ru>
2024-07-06 19:01:38 +0400

    seven commin

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import styles from "./Footer.module.scss";
 
-export default function Footer(){
+
+export default function Footer() {
     return (
-        <footer>             
-            Все права защищены &copy;
+        <footer>
+            <div className={styles.content}>
+                <div className={styles.flex}>
+                    <div className={styles.forma}>
+                        <form>
+                            <h2> Форма обращения</h2>
+                            <br />
+                            <input type="text" placeholder="Имя*" />
+                            <br /><br />
+                            <input type="email" placeholder="email*" />
+                            <br /><br />
+                            <input type="text" placeholder="Телефон*" />
+                            <br /><br />
+                            <textarea cols={30} rows={10}></textarea>
+                            <br /><br />
+                            <input type="submit" placeholder="Отправить" />
+                            <br /><br />
+                        </form>
+                    </div>
+                </div>
+                Все права защищены - 2024 &copy;
+            </div>
         </footer>
     )
 }
